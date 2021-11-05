b5d76d246364dc1690c76e889d081dba4ff8653f
Alec Meyer <11meyal@gmail.com>
2021-11-05 09:31:07 -0400

    Basic tableview

--- Firechat/Controller/ConversationsControllerViewController.swift
+++ Firechat/Controller/ConversationsControllerViewController.swift
@@ -7,14 +7,18 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsViewController: UIViewController {
 
     // MARK: - Properties
+    private let tableView = UITableView()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureTableView()
     }
     
     // MARK: - Selectors
@@ -30,6 +34,18 @@ class ConversationsViewController: UIViewController {
         
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
     }
     
     func configurateNavigationBar() {
@@ -48,5 +64,23 @@ class ConversationsViewController: UIViewController {
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
         navigationItem.title = "Messages"
         
+    }
+}
+
+extension ConversationsViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test Cell"
+        return cell
+    }
+}
+
+extension ConversationsViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
 }
